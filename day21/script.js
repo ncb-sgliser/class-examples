2bf80d84a4bb30a66ad3b4383b156ab373d37a20
ncb-sgliser <sgliser@ncblazers.org>
2022-10-24 08:45:36 -0500

    making a calculator

--- day21/script.js
+++ day21/script.js
@@ -16,6 +16,10 @@ function divide(n){
     acc = acc / n;
 }
 
+function getOpperation(el){
+    currentOpperation = el.getAttribute("data-opp");
+}
+
 function poorMansMultiply(x){
     var loopcount = 0;
     while(loopcount < x){
@@ -23,52 +27,25 @@ function poorMansMultiply(x){
         console.log("in the loop " + acc);
         loopcount++;
     }
+
+    for(loopcount = 0; loopcount < x; loopcount++){
+        add(acc);
+        console.log("in for the loop " + acc);
+    }
 }
 
 console.log("Kicking off!");
-add(1);
-sub(2);
-multiply(5);
-add(25);
-poorMansMultiply(5);
+document.getElementById("acc").textContent = acc;
 
-console.log(acc);
+var addButton = document.getElementById("add");
+var subButton = document.getElementById("sub");
+var mulButton = document.getElementById("multi");
+var divButton = document.getElementById("div");
+var eqButton = document.getElementById("equal");
+var numInput = document.getElementById("num");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//add(1,2);
-
-//pothagorean(2.0,3.0);
-
-
-console.log("Made it to the end.");
-
-
-
-function pothagorean(a, b){
-    
-    console.log( Math.pow((Math.pow(a,2) + Math.pow(b,2)), .5) )
-
-    var csquared = (Math.pow(a,2) + Math.pow(b,2));
-    console.log("c2 = " +csquared);
-
-    var c = Math.pow(csquared, .5);
-    console.log("c is "+c);
+addButton.onclick = function(){
+    console.log(this.getAttribute("data-opp"));
 }
 
-
-*/
+var currentOpperation = "";
