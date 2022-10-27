33efc6c73a25e81b0e8ab4a0d13ca548d46281be
ncb-sgliser <sgliser@ncblazers.org>
2022-10-26 22:23:28 -0500

    documenting code to make it more understandable

--- day21/script.js
+++ day21/script.js
@@ -1,5 +1,16 @@
+/**************************************
+ * Define and initialize the variables 
+ * that our program will use to act as 
+ * a calculator.
+ **************************************/
 var acc = 0;
+var currentOpperation = "";
 
+/**************************************
+ * Define the functions that our  
+ * program will use to perform basic 
+ * opperatoins.
+ **************************************/
 function add(n){
     acc = acc + n;
 }
@@ -16,27 +27,21 @@ function divide(n){
     acc = acc / n;
 }
 
-function getOpperation(el){
-    currentOpperation = el.getAttribute("data-opp");
+function getOpperation(){
+    currentOpperation = this.getAttribute("data-opp");
+    document.getElementById("currentOpp").textContent = currentOpperation;
 }
 
-function poorMansMultiply(x){
-    var loopcount = 0;
-    while(loopcount < x){
-        add(acc);
-        console.log("in the loop " + acc);
-        loopcount++;
-    }
-
-    for(loopcount = 0; loopcount < x; loopcount++){
-        add(acc);
-        console.log("in for the loop " + acc);
-    }
-}
-
+/**************************************
+ * Here is the execution portion of our 
+ * script. 
+ **************************************/
 console.log("Kicking off!");
+
+//initialize the UI display of the accumulator
 document.getElementById("acc").textContent = acc;
 
+//establish hooks to all the UI buttons
 var addButton = document.getElementById("add");
 var subButton = document.getElementById("sub");
 var mulButton = document.getElementById("multi");
@@ -44,8 +49,9 @@ var divButton = document.getElementById("div");
 var eqButton = document.getElementById("equal");
 var numInput = document.getElementById("num");
 
-addButton.onclick = function(){
-    console.log(this.getAttribute("data-opp"));
-}
+//assign the getOpperation function to click events
+addButton.onclick = getOpperation;
+subButton.onclick = getOpperation;
+mulButton.onclick = getOpperation;
+divButton.onclick = getOpperation;
 
-var currentOpperation = "";
