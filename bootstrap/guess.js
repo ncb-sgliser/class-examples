2250b649ad7bc6192c3a95a93d94446649bd01ca
Shane Gliser <sgliser@ncblazers.org>
2022-12-12 08:01:42 -0600

    latest

--- bootstrap/guess.js
+++ bootstrap/guess.js
@@ -1,3 +1,16 @@
 var guessButton = document.getElementById("submit");
 var userInput = document.getElementById("guess");
+var guesses = 6;
+
+guessButton.onclick = function(){
+    console.log(userInput.value);
+    if(guesses > 0){
+        var guessChars = userInput.value.split("");
+        for(const x in guessChars){
+            console.log(guessChars[x]);
+        }
+        console.log("check the guess");
+    }
+    guesses = guesses - 1;
+}
 
